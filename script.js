011af585b15a4faca4c6b92460944674c6c8027d
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-08-20 15:26:52 +0100

    Pulled to DOM
    Unable to figure out calling /movies/ and from there each individual category.  Was trying an approach of something like if (movies.category === "Comedy") {
    etc

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+
 window.onload = async () => {
-  
-  try {
+
+    try {
       
         const response = await fetch("https://striveschool-api.herokuapp.com/api/movies/Comedy", {
             headers: {
@@ -10,15 +11,17 @@ window.onload = async () => {
         })
         const fetchedMovies = await response.json()
         displayMovies(fetchedMovies)
-
+        displayMoviesFantasy(fetchedMovies)
+         
+        
     } catch (err) {
         console.log(err)
     }
 }
 
 const displayMovies = (movies) => {
-    const comedySection = document.getElementsByClassName(".comedyOptions")
-
+    const comedySection = document.getElementById("comedyOptions")
+    
     if (movies) {
         movies.forEach((movie) => {
           comedySection.innerHTML += ` <div class="col px-1"><a href="#"><img src="${movie.imageUrl}" alt="movie image 1"
@@ -28,3 +31,32 @@ const displayMovies = (movies) => {
     }
 
 }
+
+const displayMoviesFantasy = (movies) => {
+
+    const fantasySection = document.getElementById("fantasyOptions")
+    try {
+      
+        const response =  fetch("https://striveschool-api.herokuapp.com/api/movies/Fantasy", {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFjZjllNzJkNTI2MjAwMTViNmRjYTIiLCJpYXQiOjE2MjkzNzE5NDIsImV4cCI6MTYzMDU4MTU0Mn0.Hkx4RB6mhAb2PvDCTK6p7W2Fe7IKxKfGllbs4jWQq84",
+            }
+        })
+        
+        
+         if (movies.fantasy) {
+        movies.forEach((movie) => {
+          fantasySection.innerHTML += `<div class="col px-1"><a href="#"><img src="${movie.imageUrl}" alt="movie image 1"
+                    class="w-100 img-fluid mb-2 mb-lg-0"></a>
+            </div>`
+        })
+    }
+        
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+    
+
