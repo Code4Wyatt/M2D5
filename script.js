21268fe7cabad84df52cae987a906a77c0690ef6
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-08-27 23:43:29 +0100

    Update.

--- script.js
+++ script.js
@@ -13,29 +13,27 @@ const getMovies = async () => {
 
         if (response.ok) {
             const categories = await response.json();
-            console.log(categories)
-            const movies = Promise.all(categories.map(async (category) => {
+            console.log(categories);
+            const movies = await Promise.all(categories.map(async (category) => {
                 const result = await fetch("https://striveschool-api.herokuapp.com/api/movies/" + category,
-                {
-                    headers: {
-                        "Content-Type": "application/json",
-                        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFjZjllNzJkNTI2MjAwMTViNmRjYTIiLCJpYXQiOjE2MjkzNzE5NDIsImV4cCI6MTYzMDU4MTU0Mn0.Hkx4RB6mhAb2PvDCTK6p7W2Fe7IKxKfGllbs4jWQq84",
-                    },
-                    }
-                return await result.json();
-            };
-
-            
-            
-        })
-    };
-    console.log(movies);
-
+                    {
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFjZjllNzJkNTI2MjAwMTViNmRjYTIiLCJpYXQiOjE2MjkzNzE5NDIsImV4cCI6MTYzMDU4MTU0Mn0.Hkx4RB6mhAb2PvDCTK6p7W2Fe7IKxKfGllbs4jWQq84",
+                        },
+                    });
+                       return await result.json();
+            })
+        };
+    
+        console.log(movies);
+    }
     } catch (error) {
         console.log(error);
     }
 };
 
-window.onload = async () => {
-    await getMovies();
-}
+
+    window.onload = async () => {
+        await getMovies();
+    };
