b5de7f29c882f3399ac6897d5e8f4967c1dc7fca
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-09-03 20:54:16 +0100

    Updated JS

--- script.js
+++ script.js
@@ -1,39 +1,140 @@
+window.onload = () => {
+    getMovies();
+}
 
-const getMovies = async () => {
 
-    try {
-        const response = await fetch("https://striveschool-api.herokuapp.com/api/movies/", {
+const getMovies = async() => {
+    try { 
+        const rawData = await fetch("https://striveschool-api.herokuapp.com/api/movies/", {
             headers: {
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFjZjllNzJkNTI2MjAwMTViNmRjYTIiLCJpYXQiOjE2MzAzMzM2MDUsImV4cCI6MTYzMTU0MzIwNX0.-zeuxRckaZjZy4TaND-8fOkkra59Ebbo14uttSGPkQE",
                 "Content-Type": "application/json",
-                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFjZjllNzJkNTI2MjAwMTViNmRjYTIiLCJpYXQiOjE2MjkzNzE5NDIsImV4cCI6MTYzMDU4MTU0Mn0.Hkx4RB6mhAb2PvDCTK6p7W2Fe7IKxKfGllbs4jWQq84",
             },
+            //body: JSON.stringify(yourBody) ONLY IF NEEDED
+        });
 
-        }
-        );
-
-        if (response.ok) {
-            const categories = await response.json();
+        if (rawData.ok) {
+            const categories = await rawData.json();
             console.log(categories);
-            const movies = await Promise.all(categories.map(async (category) => {
-                const result = await fetch("https://striveschool-api.herokuapp.com/api/movies/" + category,
-                    {
+            const movies = await Promise.all(
+                categories.map(async (category) => {
+                    const res = await fetch(
+                        "https://striveschool-api.herokuapp.com/api/movies/" + category, {
                         headers: {
-                            "Content-Type": "application/json",
-                            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFjZjllNzJkNTI2MjAwMTViNmRjYTIiLCJpYXQiOjE2MjkzNzE5NDIsImV4cCI6MTYzMDU4MTU0Mn0.Hkx4RB6mhAb2PvDCTK6p7W2Fe7IKxKfGllbs4jWQq84",
+                            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFjZjllNzJkNTI2MjAwMTViNmRjYTIiLCJpYXQiOjE2MzAzMzM2MDUsImV4cCI6MTYzMTU0MzIwNX0.-zeuxRckaZjZy4TaND-8fOkkra59Ebbo14uttSGPkQE",
                         },
-                    });
-                       return await result.json();
+                    }
+                    );
+                    return await res.json();
+                })
+            );
+            console.log(movies);
+            movies.forEach(arr => {
+                console.log(arr);
+                const chunks = []
+                let k = 0
+                while (k < arr.length) {
+                    chunks.push(arr.slice(k, (k += 6)));
+                }
+                console.log("chunks:", chunks)
+                const gallery = `<div class="movie-gallery m-2">
+            <h5 class="text-light mt-2 mb-2">Trending Now</h5>
+            <div id="trending-now" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                ${chunks.map(chunk => `<div class="carousel-item active">
+                        <div class="movie-row">
+                            <div class="row">
+                            ${chunk.map(movie => `<div class="col-md-2">
+                                    <img class="movie-cover" src="./assets/media/media0.jpg">
+                                </div>`)}
+                                <div class="col-md-2">
+                                    <img class="movie-cover" src="./assets/media/media0.jpg">
+                                </div>
+                                <div class="col-md-2">
+                                    <img class="movie-cover" src="./assets/media/media1.jpg">
+                                </div>
+                                <div class="col-md-2">
+                                    <img class="movie-cover" src="./assets/media/media2.jpg">
+                                </div>
+                                <div class="col-md-2">
+                                    <img class="movie-cover" src="./assets/media/media3.jpg">
+                                </div>
+                                <div class="col-md-2">
+                                    <img class="movie-cover" src="./assets/media/media4.jpg">
+                                </div>
+                                <div class="col-md-2">
+                                    <img class="movie-cover" src="./assets/media/media5.jpg">
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>`)}
+                    <div class="carousel-item active">
+                        <div class="movie-row">
+                            <div class="row">
+                                <div class="col-md-2">
+                                    <img class="movie-cover" src="./assets/media/media0.jpg">
+                                </div>
+                                <div class="col-md-2">
+                                    <img class="movie-cover" src="./assets/media/media1.jpg">
+                                </div>
+                                <div class="col-md-2">
+                                    <img class="movie-cover" src="./assets/media/media2.jpg">
+                                </div>
+                                <div class="col-md-2">
+                                    <img class="movie-cover" src="./assets/media/media3.jpg">
+                                </div>
+                                <div class="col-md-2">
+                                    <img class="movie-cover" src="./assets/media/media4.jpg">
+                                </div>
+                                <div class="col-md-2">
+                                    <img class="movie-cover" src="./assets/media/media5.jpg">
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="movie-row">
+                            <div class="row">
+                                <div class="col-md-2">
+                                    <img class="movie-cover" src="./assets/media/media0.jpg">
+                                </div>
+                                <div class="col-md-2">
+                                    <img class="movie-cover" src="./assets/media/media1.jpg">
+                                </div>
+                                <div class="col-md-2">
+                                    <img class="movie-cover" src="./assets/media/media2.jpg">
+                                </div>
+                                <div class="col-md-2">
+                                    <img class="movie-cover" src="./assets/media/media3.jpg">
+                                </div>
+                                <div class="col-md-2">
+                                    <img class="movie-cover" src="./assets/media/media4.jpg">
+                                </div>
+                                <div class="col-md-2">
+                                    <img class="movie-cover" src="./assets/media/media5.jpg">
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#trending-now" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#trending-now" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+
+        </div>`
             })
-        };
-    
-        console.log(movies);
-    }
+        }
+       
     } catch (error) {
-        console.log(error);
+        console.error(error)
     }
-};
-
-
-    window.onload = async () => {
-        await getMovies();
-    };
+}
